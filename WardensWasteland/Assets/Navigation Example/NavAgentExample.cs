179a486ee2401af47862b6568721b13d7a5c05ae
Chewy551 <93176410+Chewy551@users.noreply.github.com>
2023-09-18 17:39:41 +0300

    Enhanced NavAgent with Jumping Capability
    Added functionality for the NavMeshAgent to handle jumping between different mesh areas using OffMeshLink. This enhancement allows game objects to seamlessly navigate complex terrains that require jumping or moving between disconnected areas. The jump trajectory is determined by an animation curve, providing flexibility in defining the jump's appearance. The update ensures smoother and more dynamic navigation for game objects in the environment.

--- WardensWasteland/Assets/Navigation Example/NavAgentExample.cs
+++ WardensWasteland/Assets/Navigation Example/NavAgentExample.cs
@@ -14,71 +14,92 @@ public class NavAgentExample : MonoBehaviour
     public bool PathPending = false;                  // Is there a path computation that is pending?
     public bool PathStale = false;                    // Is the current path stale and needs recalculating?
     public NavMeshPathStatus PathStatus = NavMeshPathStatus.PathInvalid; // Current path status.
+    public AnimationCurve JumpCurve = new AnimationCurve(); // Curve to define the jump's trajectory.
 
     // Private members
     private NavMeshAgent _navAgent = null;            // Reference to the NavMeshAgent component.
 
     void Start()
     {
-        // Get reference to the NavMeshAgent component.
+        // Initialize the NavMeshAgent.
         _navAgent = GetComponent<NavMeshAgent>();
 
-        // If no waypoint network is set, do nothing.
+        // If there's no defined waypoint network, exit the method.
         if (WaypointNetwork == null) return;
 
-        // Set the first destination for the agent.
+        // Set the initial destination for the NavMeshAgent.
         SetNextDestination(false);
     }
 
-    // Set the next destination for the NavMeshAgent.
+    // Define the next destination point for the agent to navigate to.
     void SetNextDestination(bool increment)
     {
-        // If there's no waypoint network, return.
+        // Exit if no waypoint network is defined.
         if (!WaypointNetwork) return;
 
-        // Decide which way to step through waypoints based on the 'increment' flag.
+        // Determine next waypoint index.
         int incStep = increment ? 1 : 0;
         Transform nextWaypointTransform = null;
 
-        // Find the next valid waypoint.
-        while (nextWaypointTransform == null)
+        int nextWaypoint = (CurrentIndex + incStep >= WaypointNetwork.Waypoints.Count) ? 0 : CurrentIndex + incStep;
+
+        // Fetch the transform of the next waypoint.
+        nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
+
+        // If a valid waypoint is found, set the agent's destination.
+        if (nextWaypointTransform != null)
         {
-            // Calculate the index of the next waypoint. Loop back to the beginning if end of list is reached.
-            int nextWaypoint = (CurrentIndex + incStep >= WaypointNetwork.Waypoints.Count) ? 0 : CurrentIndex + incStep;
-
-            // Fetch the transform of the next waypoint.
-            nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
-
-            // If the next waypoint is valid, set it as the new destination.
-            if (nextWaypointTransform != null)
-            {
-                CurrentIndex = nextWaypoint;
-                _navAgent.destination = nextWaypointTransform.position;
-                return;
-            }
+            CurrentIndex = nextWaypoint;
+            _navAgent.destination = nextWaypointTransform.position;
+            return;
         }
 
-        // If no valid waypoint was found, increment the current index.
+        // Increment the current waypoint index if no valid waypoint was found.
         CurrentIndex++;
     }
 
     void Update()
     {
-        // Update the path state variables.
+        // Update navigation status properties.
         HasPath = _navAgent.hasPath;
         PathPending = _navAgent.pathPending;
         PathStale = _navAgent.isPathStale;
         PathStatus = _navAgent.pathStatus;
 
-        // If the agent doesn't have a path and isn't computing one, set the next destination.
-        if ((!HasPath && !PathPending) || PathStatus==NavMeshPathStatus.PathInvalid)
+        // If the agent is on an OffMeshLink (jumping between areas), perform the jump.
+        if (_navAgent.isOnOffMeshLink)
+        {
+            StartCoroutine(Jump(1.0f));
+            return;
+        }
+
+        // Decide on the next destination based on the agent's status.
+        if ((_navAgent.remainingDistance <= _navAgent.stoppingDistance && !PathPending) || PathStatus == NavMeshPathStatus.PathInvalid)
         {
             SetNextDestination(true);
         }
-        // If the current path is stale, refresh it.
         else if (PathStale)
         {
             SetNextDestination(false);
         }
+    }
+
+    // Coroutine to animate the agent jumping between different mesh areas.
+    IEnumerator Jump(float duration)
+    {
+        OffMeshLinkData data = _navAgent.currentOffMeshLinkData;
+        Vector3 startPos = _navAgent.transform.position;
+        Vector3 endPos = data.endPos + (_navAgent.baseOffset * Vector3.up);
+        float time = 0.0f;
+
+        // Animate the agent's position over the duration using the JumpCurve.
+        while (time <= duration)
+        {
+            float t = time / duration;
+            _navAgent.transform.position = Vector3.Lerp(startPos, endPos, t) + (JumpCurve.Evaluate(t) * Vector3.up);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        _navAgent.CompleteOffMeshLink();
     }
 }
