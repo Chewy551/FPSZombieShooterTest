f968ee13739d1a8751292666b0baa0cfc1f36e5e
Chewy551 <93176410+Chewy551@users.noreply.github.com>
2023-10-08 19:49:06 +0300

    Zombie Attack Enhancements and Behavior Refinements
    Added:
    
    • Melee Zone Trigger: Introduced a new child gameobject within the player rig equipped with a trigger box. This is paired with the new MeleeZoneTrigger.cs script, facilitating close-combat engagements and interactions with zombies.
    
    • Attack Animation Layer: A specialized animation layer named Attack has been added. It's tailored with a Humanoid mask that exclusively animates the upper body, ensuring more realistic attack animations without affecting lower body movement.
    
    • AIZombieState_Attack1.cs: Deployed a new script that governs the zombie's behavior when it spots a player. The AI smoothly transitions to an Attack state when the player enters the designated melee zone, amplifying the threat zombies pose at close range.
    
    • Melee Zone Tag: To facilitate the new melee dynamics, a Melee Zone tag has been introduced. This ensures effective identification and interaction of the zone within various scripts and game mechanics.
    
    Enhancements:
    
    •AIZombieState_Pursuit1.cs Refinement: Tweaks have been made to the AIZombieState_Pursuit1.cs to enhance the visual transition between pursuit and attack states. This ensures seamless and fluid animations, making the AI behavior more believable.
    
    • Zombie Hunger Mechanics: Adjustments have been made to the AIZombieStateMachine.cs. The rate at which zombies deplete their hunger has been calibrated, ensuring consistent and logical behavior across different in-game scenarios.

--- WardensWasteland/Assets/WardensWasteland/Scripts/AI/AIZombieState_Pursuit1.cs
+++ WardensWasteland/Assets/WardensWasteland/Scripts/AI/AIZombieState_Pursuit1.cs
@@ -1,16 +1,16 @@
-﻿// ----------------------------------------------------------------------------
-// Copyright (c) 2023 [Chewy551]. All rights reserved.
-// ----------------------------------------------------------------------------
-using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.AI;
 using UnityEngine;
-using UnityEngine.AI;
+using System.Collections;
 
+// -----------------------------------------------------------------
+// Class	: AIZombieState_Pursuit1
+// Desc		: A Zombie state used for pursuing a target
+// -----------------------------------------------------------------
 public class AIZombieState_Pursuit1 : AIZombieState
 {
-    
-    // Inspector Assigned
     [SerializeField][Range(0, 10)] private float _speed = 1.0f;
+    [SerializeField][Range(0.0f, 1.0f)] float _lookAtWeight = 0.7f;
+    [SerializeField][Range(0.0f, 90.0f)] float _lookAtAngleThreshold = 15.0f;
     [SerializeField] private float _slerpSpeed = 5.0f;
     [SerializeField] private float _repathDistanceMultiplier = 0.035f;
     [SerializeField] private float _repathVisualMinDuration = 0.05f;
@@ -19,21 +19,23 @@ public class AIZombieState_Pursuit1 : AIZombieState
     [SerializeField] private float _repathAudioMaxDuration = 5.0f;
     [SerializeField] private float _maxDuration = 40.0f;
 
+
     // Private Fields
     private float _timer = 0.0f;
     private float _repathTimer = 0.0f;
+    private float _currentLookAtWeight = 0.0f;
 
+    // Mandatory Overrides
+    public override AIStateType GetStateType() { return AIStateType.Pursuit; }
 
-    public override AIStateType GetStateType()
-    {
-        return AIStateType.Pursuit;
-    }
-
+    // Default Handlers
     public override void OnEnterState()
     {
         Debug.Log("Entering Pursuit State");
+
         base.OnEnterState();
-        if (_zombieStateMachine == null) return;
+        if (_zombieStateMachine == null)
+            return;
 
         // Configure State Machine
         _zombieStateMachine.NavAgentControl(true, false);
@@ -41,28 +43,32 @@ public class AIZombieState_Pursuit1 : AIZombieState
         _zombieStateMachine.feeding = false;
         _zombieStateMachine.attackType = 0;
 
-        // Zombie will only pursue for so long before breaking off
-        _timer = 0.0f  ;
+        // Zombies will only pursue for so long before breaking off
+        _timer = 0.0f;
         _repathTimer = 0.0f;
 
-        // Set Path
+
+        // Set path
         _zombieStateMachine.navAgent.SetDestination(_zombieStateMachine.targetPosition);
-        _zombieStateMachine.navAgent.isStopped = false; 
+        _zombieStateMachine.navAgent.isStopped = false;
+
+        _currentLookAtWeight = 0.0f;
     }
 
+    // ---------------------------------------------------------------------
+    // Name	:	OnUpdateAI
+    // Desc	:	The engine of this state
+    // ---------------------------------------------------------------------
     public override AIStateType OnUpdate()
     {
         _timer += Time.deltaTime;
         _repathTimer += Time.deltaTime;
 
-        // If we are in Pursuit for longer than our MaxDuration then switch to Patrol State
         if (_timer > _maxDuration)
-        {
             return AIStateType.Patrol;
-        }
 
-        // If we are chasing the player and have entered the melee trigger then switch to Attack State
-        if (_zombieStateMachine.targetType == AITargetType.Visual_Player && _zombieStateMachine.inMeleeRange)
+        // IF we are chasing the player and have entered the melee trigger then attack
+        if (_stateMachine.targetType == AITargetType.Visual_Player && _zombieStateMachine.inMeleeRange)
         {
             return AIStateType.Attack;
         }
@@ -70,68 +76,68 @@ public class AIZombieState_Pursuit1 : AIZombieState
         // Otherwise this is navigation to areas of interest so use the standard target threshold
         if (_zombieStateMachine.isTargetReached)
         {
-            switch (_zombieStateMachine.targetType)
+            switch (_stateMachine.targetType)
             {
-                case AITargetType.Visual_Light:
+
+                // If we have reached the source
                 case AITargetType.Audio:
-                    _zombieStateMachine.ClearTarget(); // Clear the threat
-                    return AIStateType.Alerted; // Become alert and scan for targets
+                case AITargetType.Visual_Light:
+                    _stateMachine.ClearTarget();    // Clear the threat
+                    return AIStateType.Alerted;     // Become alert and scan for targets
 
                 case AITargetType.Visual_Food:
+                    Debug.Log("Food Reached");
                     return AIStateType.Feeding;
             }
         }
 
-        // If for any reason the nav agent has lost its path then call the event to say we are not longer pursuing
+
+        // If for any reason the nav agent has lost its path then call then drop into alerted state
+        // so it will try to re-aquire the target or eventually giveup and resume patrolling
         if (_zombieStateMachine.navAgent.isPathStale ||
-            (!_zombieStateMachine.navAgent.hasPath && !_zombieStateMachine.navAgent.pathPending)||
+            (!_zombieStateMachine.navAgent.hasPath && !_zombieStateMachine.navAgent.pathPending) ||
             _zombieStateMachine.navAgent.pathStatus != NavMeshPathStatus.PathComplete)
         {
             return AIStateType.Alerted;
         }
 
-        // If there is no path set speed to 0, otherwise set it to speed and do rotate statement.
         if (_zombieStateMachine.navAgent.pathPending)
-        {
             _zombieStateMachine.speed = 0;
-        }
         else
         {
             _zombieStateMachine.speed = _speed;
 
-            // If we are close to the target then turn off the nav agent
-            if (!_zombieStateMachine.useRootRotation &&
-                _zombieStateMachine.targetType == AITargetType.Visual_Light &&
-                _zombieStateMachine.VisualThreat.type == AITargetType.Visual_Player &&
-                _zombieStateMachine.isTargetReached)
+            // If we are close to the target that was a player and we still have the player in our vision then keep facing right at the player
+            if (!_zombieStateMachine.useRootRotation && _zombieStateMachine.targetType == AITargetType.Visual_Player && _zombieStateMachine.VisualThreat.type == AITargetType.Visual_Player && _zombieStateMachine.isTargetReached)
             {
                 Vector3 targetPos = _zombieStateMachine.targetPosition;
                 targetPos.y = _zombieStateMachine.transform.position.y;
                 Quaternion newRot = Quaternion.LookRotation(targetPos - _zombieStateMachine.transform.position);
                 _zombieStateMachine.transform.rotation = newRot;
             }
-            // Slowly update our rotation to match the nav agents desired rotation BUT only if we are not persuing the player and are not in melee range
-            else if (!_zombieStateMachine.useRootRotation && !_zombieStateMachine.isTargetReached)
+            else
+            // SLowly update our rotation to match the nav agents desired rotation BUT only if we are not persuing the player and are really close to him
+            if (!_stateMachine.useRootRotation && !_zombieStateMachine.isTargetReached)
             {
                 // Generate a new Quaternion representing the rotation we should have
                 Quaternion newRot = Quaternion.LookRotation(_zombieStateMachine.navAgent.desiredVelocity);
+
                 // Smoothly rotate to that new rotation over time
                 _zombieStateMachine.transform.rotation = Quaternion.Slerp(_zombieStateMachine.transform.rotation, newRot, Time.deltaTime * _slerpSpeed);
             }
-            // If we have reached the target location and there is nothing there, drop to alerted state and look for targets.
             else if (_zombieStateMachine.isTargetReached)
             {
                 return AIStateType.Alerted;
             }
         }
-        
+
         // Do we have a visual threat that is the player
         if (_zombieStateMachine.VisualThreat.type == AITargetType.Visual_Player)
         {
             // The position is different - maybe same threat but it has moved so repath periodically
             if (_zombieStateMachine.targetPosition != _zombieStateMachine.VisualThreat.position)
             {
-                // Repath more frequently as we get closer to the target - helps with slime mould effect
+                // Repath more frequently as we get closer to the target (try and save some CPU cycles)
                 if (Mathf.Clamp(_zombieStateMachine.VisualThreat.distance * _repathDistanceMultiplier, _repathVisualMinDuration, _repathVisualMaxDuration) < _repathTimer)
                 {
                     // Repath the agent
@@ -139,7 +145,6 @@ public class AIZombieState_Pursuit1 : AIZombieState
                     _repathTimer = 0.0f;
                 }
             }
-
             // Make sure this is the current target
             _zombieStateMachine.SetTarget(_zombieStateMachine.VisualThreat);
 
@@ -148,34 +153,36 @@ public class AIZombieState_Pursuit1 : AIZombieState
         }
 
         // If our target is the last sighting of a player then remain
-        // in pursuit mode until we have the player again
+        // in pursuit as nothing else can override
         if (_zombieStateMachine.targetType == AITargetType.Visual_Player)
-        {
             return AIStateType.Pursuit;
-        }
+
+
+
 
         // If we have a visual threat that is the player's light
         if (_zombieStateMachine.VisualThreat.type == AITargetType.Visual_Light)
         {
-            // and we currently have a lower priority target tjem drop into alerted
+            // and we currently have a lower priority target then drop into alerted
             // mode and try to find source of light
             if (_zombieStateMachine.targetType == AITargetType.Audio || _zombieStateMachine.targetType == AITargetType.Visual_Food)
             {
                 _zombieStateMachine.SetTarget(_zombieStateMachine.VisualThreat);
                 return AIStateType.Alerted;
             }
-            else if (_zombieStateMachine.targetType == AITargetType.Visual_Food)
+            else
+            if (_zombieStateMachine.targetType == AITargetType.Visual_Light)
             {
                 // Get unique ID of the collider of our target
                 int currentID = _zombieStateMachine.targetColliderID;
 
-                // If this is the same light that we are currently targeting
+                // If this is the same light
                 if (currentID == _zombieStateMachine.VisualThreat.collider.GetInstanceID())
                 {
                     // The position is different - maybe same threat but it has moved so repath periodically
                     if (_zombieStateMachine.targetPosition != _zombieStateMachine.VisualThreat.position)
                     {
-                        // Repath more frequently as we get closer to the target - helps with slime mould effect
+                        // Repath more frequently as we get closer to the target (try and save some CPU cycles)
                         if (Mathf.Clamp(_zombieStateMachine.VisualThreat.distance * _repathDistanceMultiplier, _repathVisualMinDuration, _repathVisualMaxDuration) < _repathTimer)
                         {
                             // Repath the agent
@@ -184,40 +191,38 @@ public class AIZombieState_Pursuit1 : AIZombieState
                         }
                     }
 
-                    // Make sure this is the current target
                     _zombieStateMachine.SetTarget(_zombieStateMachine.VisualThreat);
-
-                    // Remain in pursuit state
                     return AIStateType.Pursuit;
                 }
                 else
                 {
-                    // Switch to new light
                     _zombieStateMachine.SetTarget(_zombieStateMachine.VisualThreat);
                     return AIStateType.Alerted;
                 }
             }
-
         }
-        else if (_zombieStateMachine.AudioThreat.type == AITargetType.Audio)
+        else
+        if (_zombieStateMachine.AudioThreat.type == AITargetType.Audio)
         {
+
             if (_zombieStateMachine.targetType == AITargetType.Visual_Food)
             {
                 _zombieStateMachine.SetTarget(_zombieStateMachine.AudioThreat);
                 return AIStateType.Alerted;
             }
-            else if (_zombieStateMachine.targetType == AITargetType.Audio)
+            else
+            if (_zombieStateMachine.targetType == AITargetType.Audio)
             {
                 // Get unique ID of the collider of our target
                 int currentID = _zombieStateMachine.targetColliderID;
 
-                // If this is the same light that we are currently targeting
+                // If this is the same light
                 if (currentID == _zombieStateMachine.AudioThreat.collider.GetInstanceID())
                 {
                     // The position is different - maybe same threat but it has moved so repath periodically
                     if (_zombieStateMachine.targetPosition != _zombieStateMachine.AudioThreat.position)
                     {
-                        // Repath more frequently as we get closer to the target - helps with slime mould effect
+                        // Repath more frequently as we get closer to the target (try and save some CPU cycles)
                         if (Mathf.Clamp(_zombieStateMachine.AudioThreat.distance * _repathDistanceMultiplier, _repathAudioMinDuration, _repathAudioMaxDuration) < _repathTimer)
                         {
                             // Repath the agent
@@ -226,22 +231,39 @@ public class AIZombieState_Pursuit1 : AIZombieState
                         }
                     }
 
-                    // Make sure this is the current target
                     _zombieStateMachine.SetTarget(_zombieStateMachine.AudioThreat);
-
-                    // Remain in pursuit state
                     return AIStateType.Pursuit;
                 }
                 else
                 {
-                    // Switch to new audio source
                     _zombieStateMachine.SetTarget(_zombieStateMachine.AudioThreat);
                     return AIStateType.Alerted;
                 }
             }
         }
 
+        // Default
         return AIStateType.Pursuit;
     }
+    // -----------------------------------------------------------------------
+    // Name	:	OnAnimatorIKUpdated
+    // Desc	:	Override IK Goals
+    // -----------------------------------------------------------------------
+    public override void OnAnimatorIKUpdated()
+    {
+        if (_zombieStateMachine == null)
+            return;
 
+        if (Vector3.Angle(_zombieStateMachine.transform.forward, _zombieStateMachine.targetPosition - _zombieStateMachine.transform.position) < _lookAtAngleThreshold)
+        {
+            _zombieStateMachine.animator.SetLookAtPosition(_zombieStateMachine.targetPosition + Vector3.up);
+            _currentLookAtWeight = Mathf.Lerp(_currentLookAtWeight, _lookAtWeight, Time.deltaTime);
+            _zombieStateMachine.animator.SetLookAtWeight(_currentLookAtWeight);
+        }
+        else
+        {
+            _currentLookAtWeight = Mathf.Lerp(_currentLookAtWeight, 0.0f, Time.deltaTime);
+            _zombieStateMachine.animator.SetLookAtWeight(_currentLookAtWeight);
+        }
+    }
 }
